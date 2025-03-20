6698747e4fa754b12fe65498a43f675e9f2ef369
LucasCarvalhoo <lucas.carvalho7933@gmail.com>
2025-03-19 22:27:51 -0300

    commit inicial

---
+++ src/main/java/com/carvalho/patinho_feio/PatinhoFeioApplication.java
@@ -0,0 +1,13 @@
+package com.carvalho.patinho_feio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatinhoFeioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatinhoFeioApplication.class, args);
+	}
+
+}
